2a23ce1327decd8d69dddc0ca71750eaef14f849
Benjamin Matthews <ben@kc2vjw.com>
2011-09-11 04:21:52 -0600

    test Cmake's C<->Fortran Interface

---
+++ test.f
@@ -0,0 +1,8 @@
+      program test
+      real x
+      x = 5
+      x = calculate_x(x)
+      write (*,*) 'x is ', x
+
+      stop
+      end
---
+++ testc.c
@@ -0,0 +1,6 @@
+#include <FCMangle.h>
+
+float calculate_x()
+{
+	return 6.42;
+}
