853420a0e8ab8eaf3ae22f4a135918b196de47df
Ben Matthews <ben@kc2vjw.com>
2012-03-27 14:24:51 -0600

    test 3d array accesses

--- test.f
+++ test.f
@@ -1,8 +1,28 @@
       program test
-      real x
+      real*8, allocatable :: threedarray(:,:,:)
+      real*4 x
+      integer boundx
+      integer boundy
+      integer boundz
+      integer l
+      boundx = 10
+      boundy = 11
+      boundz = 12
+      l=1
       x = 5
       x = calculate_x(x)
+      allocate(threedarray(boundx,boundy,boundz))
       write (*,*) 'x is ', x
-
+!      write (*,*) 'Fortran!'
+      do i = 1, boundx
+          do j = 1, boundy
+              do k = 1, boundz
+                  threedarray(i,j,k) = l
+!                  write(*,*) l
+                  l=l+1
+              enddo
+          enddo
+      enddo
+      call access_array(threedarray,boundx,boundy,boundz,l)
       stop
       end
--- testc.c
+++ testc.c
@@ -1,6 +1,30 @@
+#include <stdio.h>
+#include <assert.h>
 #include <FCMangle.h>
+#define access_array FC_GLOBAL_(access_array,ACCESS_ARRAY)
 
 float calculate_x()
 {
 	return 6.42;
+}
+#define COLMAJ3D(a,b,c,amax,bmax,cmax) a+amax*(b+bmax*c)
+void access_array(double* array,int* boundx,int* boundy,int* boundz, int* lmax)
+{
+	int i,j,k,l=1;
+	//printf("C!\n");
+	for(i=0;i<*boundx;i++)
+	{
+		for(j=0;j<*boundy;j++)
+		{
+			for(k=0;k<*boundz;k++)
+			{
+				assert(
+				  array[COLMAJ3D(i,j,k,*boundx,*boundy,*boundz)]
+				  == l);
+				//printf("%d\n",l);
+				l++;
+			}
+		}
+	}
+	assert(l==*lmax);
 }
